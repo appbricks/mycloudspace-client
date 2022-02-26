1381e1a0d43f21af72af3feb56e99a1123e6a46c
Mevan Samaratunga <mevansam@gmail.com>
2022-02-25 22:02:54 -0500

    added api calls to query and update user attribs

--- mycscloud/user.go
+++ mycscloud/user.go
@@ -1,1 +1,91 @@
 package mycscloud
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/appbricks/cloud-builder/userspace"
+	"github.com/hasura/go-graphql-client"
+	"github.com/mevansam/goutils/logger"
+)
+
+type UserAPI struct {
+	apiClient *graphql.Client
+}
+
+func NewUserAPI(apiClient *graphql.Client) *UserAPI {
+
+	return &UserAPI{
+		apiClient: apiClient,
+	}
+}
+
+func (u *UserAPI) GetUserConfig(user *userspace.User) (string, error) {
+
+	var query struct {
+		GetUser struct {
+			UserID          graphql.String `graphql:"userID"`
+			PublicKey       graphql.String
+			Certificate     graphql.String
+			UniversalConfig graphql.String
+		} `graphql:"getUser"`
+	}
+	if err := u.apiClient.Query(context.Background(), &query, map[string]interface{}{}); err != nil {
+		logger.DebugMessage("UserAPI: getUsers query to retrieve user returned an error: %s", err.Error())
+		return "", err
+	}
+	logger.TraceMessage("UserAPI: getUsers query to retrieve user returned response: %# v", query)
+
+	if (user.UserID != string(query.GetUser.UserID)) {
+		return "", fmt.Errorf("getUsers returned user does not match given user")
+	}
+	user.RSAPublicKey = string(query.GetUser.PublicKey)
+	user.Certificate = string(query.GetUser.Certificate)
+	return string(query.GetUser.UniversalConfig), nil
+}
+
+func (u *UserAPI) UpdateUserKey(user *userspace.User) error {
+
+	var mutation struct {
+		UpdateUserKey struct {
+			UserID graphql.String `graphql:"userID"`
+		} `graphql:"updateUserKey(userKey: { publicKey: $publicKey, keyTimestamp: $keyTimestamp })"`
+	}
+	variables := map[string]interface{}{
+		"publicKey": graphql.String(user.RSAPublicKey),
+		"keyTimestamp": graphql.String(strconv.FormatInt(user.KeyTimestamp, 10)),
+	}
+	if err := u.apiClient.Mutate(context.Background(), &mutation, variables); err != nil {
+		logger.DebugMessage("UserAPI: updateUserKey mutation returned an error: %s", err.Error())
+		return err
+	}
+	logger.TraceMessage("UserAPI: updateUserKey mutation returned response: %# v", mutation)
+
+	if (user.UserID != string(mutation.UpdateUserKey.UserID)) {
+		return fmt.Errorf("updateUserKey returned user does not match given user")
+	}
+	return nil
+}
+
+func (u *UserAPI) UpdateUserConfig(user *userspace.User, configData []byte) error {
+
+	var mutation struct {
+		UpdateUserConfig struct {
+			UserID graphql.String `graphql:"userID"`
+		} `graphql:"updateUserConfig(universalConfig: $config)"`
+	}
+	variables := map[string]interface{}{
+		"config": graphql.String(configData),
+	}
+	if err := u.apiClient.Mutate(context.Background(), &mutation, variables); err != nil {
+		logger.DebugMessage("UserAPI: updateUserConfig mutation returned an error: %s", err.Error())
+		return err
+	}
+	logger.TraceMessage("UserAPI: updateUserConfig mutation returned response: %# v", mutation)
+
+	if (user.UserID != string(mutation.UpdateUserConfig.UserID)) {
+		return fmt.Errorf("updateUserConfig returned user does not match given user")
+	}
+	return nil
+}
--- mycscloud/user_test.go
+++ mycscloud/user_test.go
@@ -1,1 +1,178 @@
 package mycscloud_test
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/appbricks/cloud-builder/config"
+	"github.com/appbricks/cloud-builder/userspace"
+	"github.com/appbricks/mycloudspace-client/api"
+	"github.com/appbricks/mycloudspace-client/mycscloud"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	mycs_mocks "github.com/appbricks/mycloudspace-client/test/mocks"
+	"github.com/mevansam/goutils/crypto"
+	test_server "github.com/mevansam/goutils/test/mocks"
+)
+
+var _ = Describe("User API", func() {
+
+	var (
+		err error
+
+		cfg        config.Config
+		testServer *test_server.MockHttpServer
+
+		userAPI *mycscloud.UserAPI
+	)
+
+	BeforeEach(func() {
+		cfg, err = mycs_mocks.NewMockConfig(sourceDirPath)
+		Expect(err).NotTo(HaveOccurred())
+		
+		// start test server
+		testServer = test_server.NewMockHttpServer(9096)
+		testServer.ExpectCommonHeader("Authorization", "mock authorization token")
+		testServer.Start()
+
+		// user API client
+		userAPI = mycscloud.NewUserAPI(api.NewGraphQLClient("http://localhost:9096/", "", cfg))
+	})
+
+	AfterEach(func() {
+		testServer.Stop()
+	})
+
+	It("retrieves a user", func() {
+
+		user := &userspace.User{
+			UserID: "test user id x",
+		}
+
+		testServer.PushRequest().
+			ExpectJSONRequest(getUserRequest).
+			RespondWith(errorResponse)
+
+		_, err = userAPI.GetUserConfig(user)
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(Equal("an error occurred"))
+
+		testServer.PushRequest().
+			ExpectJSONRequest(getUserRequest).
+			RespondWith(getUserResponse)
+
+		_, err := userAPI.GetUserConfig(user)
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(Equal("returned user does not match given user"))
+
+		testServer.PushRequest().
+			ExpectJSONRequest(getUserRequest).
+			RespondWith(getUserResponse)
+
+		user.UserID = "test user id"
+		config, err := userAPI.GetUserConfig(user)
+		Expect(err).ToNot(HaveOccurred())
+		
+		Expect(config).To(Equal("test config data"))
+		Expect(testServer.Done()).To(BeTrue())
+	})
+
+	It("updates a user's key", func() {
+
+		timestamp := time.Now().UnixMilli()
+		user := &userspace.User{
+			UserID: "test user id",
+			RSAPublicKey: "test public key",
+			KeyTimestamp: timestamp,
+		}
+
+		request := fmt.Sprintf(updateUserKeyRequest, timestamp)
+
+		testServer.PushRequest().
+			ExpectJSONRequest(request).
+			RespondWith(errorResponse)
+
+		err = userAPI.UpdateUserKey(user)
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(Equal("an error occurred"))
+
+		testServer.PushRequest().
+			ExpectJSONRequest(request).
+			RespondWith(updateUserKeyResponse)
+
+		err = userAPI.UpdateUserKey(user)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	It("updates a user's config", func() {
+
+		key, err := crypto.NewRSAKey()
+		Expect(err).ToNot(HaveOccurred())
+
+		user := &userspace.User{
+			UserID: "test user id",
+		}
+		err = user.SetKey(key)
+		Expect(err).ToNot(HaveOccurred())
+
+		testServer.PushRequest().
+			ExpectJSONRequest(updateUserConfigRequest).
+			RespondWith(errorResponse)
+
+		err = userAPI.UpdateUserConfig(user, []byte("test config data"))
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(Equal("an error occurred"))
+
+		testServer.PushRequest().
+			ExpectJSONRequest(updateUserConfigRequest).
+			RespondWith(updateUserConfigResponse)
+
+		err = userAPI.UpdateUserConfig(user, []byte("test config data"))
+		Expect(err).ToNot(HaveOccurred())
+	})
+})
+
+const getUserRequest = `{
+	"query": "{getUser{userID,publicKey,certificate,universalConfig}}"
+}`
+const getUserResponse = `{
+	"data": {
+		"getUser": {
+			"userID": "test user id",
+			"publicKey": "test public key",
+			"certificate": "test certificate",
+			"universalConfig": "test config data"
+		}
+	}
+}`
+
+const updateUserKeyRequest = `{
+  "query": "mutation ($keyTimestamp:String!$publicKey:String!){updateUserKey(userKey: { publicKey: $publicKey, keyTimestamp: $keyTimestamp }){userID}}",
+  "variables": {
+    "keyTimestamp": "%d",
+    "publicKey": "test public key"
+  }
+}`
+const updateUserKeyResponse = `{
+	"data": {
+		"updateUserKey": {
+			"userID": "test user id"
+		}
+	}
+}`
+
+const updateUserConfigRequest = `{
+  "query": "mutation ($config:String!){updateUserConfig(universalConfig: $config){userID}}",
+  "variables": {
+    "config": "test config data"
+  }
+}`
+const updateUserConfigResponse = `{
+	"data": {
+		"updateUserConfig": {
+			"userID": "test user id"
+		}
+	}
+}`
