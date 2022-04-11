872a3c3dc09d625656712193d8fa173e56d8217d
Mevan Samaratunga <mevansam@gmail.com>
2022-04-11 11:07:51 -0400

    added device info for linux and windows

--- system/device_info_linux.go
+++ system/device_info_linux.go
@@ -2,3 +2,62 @@
 // +build linux
 
 package system
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/mevansam/goutils/logger"
+	"github.com/mevansam/goutils/run"
+	"github.com/mitchellh/go-homedir"
+)
+
+func init() {
+
+	// asynchronously determine device type 
+	// via available system utilities
+	go func() {
+		var (
+			err error
+	
+			systemDetectVirt run.CLI
+			outputBuffer     bytes.Buffer	
+		)		
+		
+		deviceType := "LinuxPC"
+		home, _ := homedir.Dir()
+
+		if systemDetectVirt, err = run.NewCLI("/usr/bin/systemd-detect-virt", home, &outputBuffer, &outputBuffer); err != nil {
+			logger.ErrorMessage("system.init:linux(): Error creating CLI for /usr/bin/systemd-detect-virt: %s", err.Error())
+			deviceTypeC <- deviceType
+			return
+		}
+		if err = systemDetectVirt.Run([]string{}); err != nil {
+			logger.ErrorMessage("system.init:linux(): Error running \"systemd-detect-virt\": %s", err.Error())
+			deviceTypeC <- deviceType
+			return
+		}
+		
+		virtType := strings.TrimSpace(outputBuffer.String())
+		switch virtType {
+		case "none":
+			deviceTypeC <- deviceType
+		case "oracle":
+			deviceTypeC <- "LinuxVM/virtualbox"
+		case "openvz",
+			"lxc", 
+			"lxc-libvirt", 
+			"systemd-nspawn",
+			"docker", 
+			"podman",
+			"rkt",
+			"wsl",
+			"proot",
+			"pouch":
+			deviceTypeC <- fmt.Sprintf("LinuxContainer/%s", virtType)
+		default:
+			deviceTypeC <- fmt.Sprintf("LinuxVM/%s", virtType)
+		}
+	}()
+}
--- system/device_info_windows.go
+++ system/device_info_windows.go
@@ -2,3 +2,73 @@
 // +build windows
 
 package system
+
+import (
+	"bytes"
+	"strings"
+	"regexp"
+
+	"github.com/mevansam/goutils/logger"
+	"github.com/mevansam/goutils/run"
+	"github.com/mevansam/goutils/utils"
+	"github.com/mitchellh/go-homedir"
+)
+
+var (
+	systemModelPattern  = regexp.MustCompile(`^System Model:\s+(.*)\s*$`)
+	hyperVReqmtsPattern = regexp.MustCompile(`^Hyper-V Requirements:\s+(.*)\s*$`)
+)
+
+func init() {
+
+	// asynchronously determine device type 
+	// via available system utilities
+	go func() {
+		var (
+			err error
+			
+			systemInfo   run.CLI
+			outputBuffer bytes.Buffer	
+
+			results map[string][][]string
+
+			systemModel, hyperVReqmts string
+		)
+		
+		deviceType := "WindowsPC"
+		home, _ := homedir.Dir()
+
+		if systemInfo, err = run.NewCLI("C:/Windows/System32/systeminfo.exe", home, &outputBuffer, &outputBuffer); err != nil {
+			logger.ErrorMessage("system.init:windows(): Error creating CLI for C:/Windows/System32/systeminfo.exe: %s", err.Error())
+			deviceTypeC <- deviceType
+			return
+		}
+		if err = systemInfo.Run([]string{}); err != nil {
+			logger.ErrorMessage("system.init:windows(): Error running \"systeminfo.exe\": %s", err.Error())
+			deviceTypeC <- deviceType
+			return
+		}
+		results = utils.ExtractMatches(outputBuffer.Bytes(), map[string]*regexp.Regexp{
+			"systemModel": systemModelPattern,
+			"hyperVReqmts": hyperVReqmtsPattern,
+		})
+		if results["systemModel"] != nil && len(results["systemModel"][0]) == 2 {
+			systemModel = results["systemModel"][0][1]
+		}
+		if results["hyperVReqmts"] != nil && len(results["hyperVReqmts"][0]) == 2 {
+			hyperVReqmts = results["hyperVReqmts"][0][1]
+		}
+
+		if strings.HasPrefix(hyperVReqmts, "A hypervisor has been detected.") {
+			if strings.HasPrefix(systemModel, "VirtualBox") {
+				deviceTypeC <- "WindowsVM/VirtualBox"
+			}	else if strings.HasPrefix(systemModel, "VMware") {
+				deviceTypeC <- "WindowsVM/VMware"
+			} else {
+				deviceTypeC <- "WindowsVM"
+			}	
+		} else {
+			deviceTypeC <- deviceType
+		}
+	}()
+}
